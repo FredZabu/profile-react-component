85c53d2e8fb4f9e0e2c7cb0171832f85e61a7d66
Fred Zabulon <fredzabulon133@gmail.com>
2022-11-13 22:09:29 +0300

    Finalised with the component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Profile from './components/Profile';
 
 function App() {
   return (
-      <div className='w-1/4 mx-auto my-20 p-2 bg-white'>
+      <div className='w-1/5 mx-auto my-20 p-4 bg-white'>
           <Profile />
     </div>
   )
---
+++ src/Data/person.js
@@ -0,0 +1,60 @@
+import pic1 from '../Assets/pic1.jpg';
+import pic2 from '../Assets/pic2.jpg';
+import pic3 from '../Assets/pic3.jpg';
+import pic4 from '../Assets/pic4.jpg';
+import pic5 from '../Assets/pic5.jpg';
+import pic6 from '../Assets/pic1.jpg';
+import pic7 from '../Assets/pic2.jpg';
+import pic8 from '../Assets/pic3.jpg';
+import pic9 from '../Assets/pic4.jpg';
+import profilePic from '../Assets/profile.jpg';
+
+const PERSON = {
+    profileImage: profilePic,
+    personName: "Claudia Amanda",
+    job: "interface Designer @Aceel",
+    email: "mucapp.com/clamanda",
+    posts: 200,
+    followers: "9k",
+    following: 525,
+    personMediaFiles: [
+        {
+            photo: pic1,
+            id: 1
+        },
+         {
+            photo: pic2,
+            id: "2"
+        },
+          {
+            photo: pic3,
+            id: "3"
+        },
+           {
+            photo: pic4,
+            id: "4"
+        },
+            {
+            photo: pic5,
+            id: "5"
+        },
+             {
+            photo: pic6,
+            id: "6"
+        },
+              {
+            photo: pic7,
+            id: "7"
+        },
+               {
+            photo: pic8,
+            id: "8"
+        },
+                {
+            photo: pic9,
+            id: "9"
+        }
+    ],
+}
+
+export default PERSON;
